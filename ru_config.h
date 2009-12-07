78426d29b30a11ff1cb526eb7d6333a173b7672f
Owen Healy <ellbur@gmail.com>
2009-12-07 00:44:37 -0500

    Thought this might be handy: global configuration

---
+++ ru_config.h
@@ -0,0 +1,9 @@
+
+#ifndef _ru_config_h
+#define _ru_config_h 1
+
+#define BUTTON_REV_THRESH       ((int)100)
+#define BUTTON_FWD_THRESH       ((int)154)
+#define NEUTRAL_VALUE           ((int)127)
+
+#endif
---
+++ ru_io_aliases.h
@@ -0,0 +1,14 @@
+
+#ifndef _ru_io_aliases_h
+#define _ru_io_aliases_h 1
+
+#define IR_SENSOR         rc_ana_in02
+#define IR_SENSOR_1       rc_ana_in02
+#define IR_SENSOR_2       rc_ana_in04
+#define RESISTOR          rc_ana_in06
+
+#define MOTOR             pwm01
+
+#define MOTOR_SWITCH      PWM_in6
+
+#endif
